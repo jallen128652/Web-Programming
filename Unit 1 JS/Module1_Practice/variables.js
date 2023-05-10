f3b697919db6361c5839915162fe51179a8ec104
jallen128652 <jallen128652@mymail.tstc.edu>
2023-05-10 10:53:03 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   Unit 1 JS/Module1_Practice/images/Variables - Brave 5_10_2023 10_50_12 AM.png
    	new file:   Unit 1 JS/Module1_Practice/index.html
    	new file:   Unit 1 JS/Module1_Practice/variables.js

---
+++ Unit 1 JS/Module1_Practice/variables.js
@@ -0,0 +1,11 @@
+var webDev = "The Web Design and Development Department at Texas State Technical College prepares students to design and develop responsive websites for Texas businesses and non-profits.";
+var greet = "Hello world!";
+var myFn = "James";
+var myLn = "Allen";
+var helloW = "\"     Hello World!      \"";
+var irisJohnsonBooks = "The Face Of Deception, The Killing Game, The Search, Body Of Lies, Blind Alley, Countdown, Stalemate, Quicksand, Blood Game, Eight Days To Live, Chasing The Night, Eve, Quinn, Bonnie, Sleep No More, Taking Eve, Hunting Eve, Silencing Eve, Shadow Play, Hide Away, Night And Day, Mind Game, Shattered Mirror, Vendetta, Dark Tribute, Smokescreen";
+var isString = "10";
+var question = true;
+var num = 10;
+var num1 = 8.2567;
+
