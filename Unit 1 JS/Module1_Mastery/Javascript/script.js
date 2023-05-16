4f2e05437f53adce3387ee38d11cffc3d37378c1
jallen128652 <jallen128652@mymail.tstc.edu>
2023-05-15 21:51:51 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Unit 1 JS/Module1_Mastery/Javascript/script.js
    	deleted:    Unit 1 JS/Module1_Mastery/script.js

--- Unit 1 JS/Module1_Mastery/Javascript/script.js
+++ Unit 1 JS/Module1_Mastery/Javascript/script.js
@@ -13,13 +13,14 @@ var num3 = 25;
 // console commands
 
 // concat first and last name
-myFn + " " + myLn;
+console.log(myFn + " " + myLn);
 
 // sentence display and length
-"The length of my sentence is " + sentence.length + " characters long.";
+console.log("The length of my sentence is " + sentence.length + " characters long.");
 
 // concat nums
-"The sum of " + num + " and " + num2 + " is equal to " + num3 + ".";
+console.log("The sum of " + num + " and " + num2 + " is equal to " + num3 + ".");
 
 // data type concat string
-"The variable theTruth is storing the value " + theTruth + ". The data type of theTruth is " + typeof(theTruth) + ".";
+console.log("The variable theTruth is storing the value " + theTruth + ". The data type of theTruth is " + typeof(theTruth) + ".");
+
