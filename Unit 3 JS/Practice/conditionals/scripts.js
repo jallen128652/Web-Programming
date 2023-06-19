f68909b0af6755acf908f9ec1856c6801f87eef3
jallen128652 <jallen128652@mymail.tstc.edu>
2023-06-19 14:10:04 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Unit 3 JS/Practice/conditionals/scripts.js

--- Unit 3 JS/Practice/conditionals/scripts.js
+++ Unit 3 JS/Practice/conditionals/scripts.js
@@ -198,3 +198,121 @@ var animal = "cat";
 animal === "cat" ? console.log("You will be a cat herder") : console.log("You will be a dog herder");
 // additional shorthand single line with variable
 var job = animal === "cat" ? "cat herder" : "dog catcher";
+
+// --------------------------------practice---------------------------
+//1. Create a variable called name and assign your name to it. Write an if condition to determine if the name variable is the same as your name written as a string "Your name". (comparsion operator) If it is true the body should write out: "The name is the same."
+//Write code below:
+var name1 = "Jim";
+if (name1 === "Jim") {
+    console.log("The name is the same.");
+}
+
+//2. Duplicate your code above and change the first character of your name to a lower case and check the console for the result.
+//Write code below:
+var name1 = "Jim";
+if (name1 === "jim") {
+    console.log("The name is the same.");
+}
+
+//3. If you did the code above correctly, the console didn't print anything to it. Now duplicate the code again and this time add an else statement. The body for the else should print to the console: "The name was not the same."
+//Write code below:
+var name1 = "Jim";
+if (name1 === "jim") {
+    console.log("The name is the same.");
+} else {
+    console.log("The name was not the same.");
+}
+
+//4. Duplicate the code and try using the !== comparison symbol. Watch the console for the output. Think about the routing and why it does what it does.
+//Write code below:
+var name1 = "Jim";
+if (name1 !== "jim") {
+    console.log("The name is the same.");
+} else {
+    console.log("The name was not the same.");
+}
+
+//5. Uncomment the variables below. Write a conditional statement to check if num1 is larger then num2. If it is larger, the answer variable should contain the value when you take num1 minus num2. Print out the answer to the console using the sentence: "The answer is: " + answer
+//Write code below:
+var num1 = 10;
+var num2 = 3;
+var answer;
+
+if (num1 > num2) {
+    answer = num1 - num2;
+    console.log("The answer is: " + answer);
+}
+
+//6. Duplicate the code above, change the value of num1 equals 6 and num2 equals 4. Add an else-if condition to see if num1 is less then num2. Configure the math to present the answer the same for either if conditions. Swap the values for num1 and num2 to check your answers.
+//Write code below:
+var num1 = 4;
+var num2 = 6;
+var answer;
+
+if (num1 > num2) {
+    answer = num1 - num2;
+    console.log("The answer is: " + answer);
+} else if (num1 < num2) {
+    answer = num2 - num1;
+    console.log("The answer is: " + answer);
+}
+//7. Duplicate the code above, add an else to your code. The else should assign the answer variable with string that says: "neither". Alter the code to only allow the else to run. You can do this several different ways, so don't be afraid to modify anything.
+//Write code below:
+var num1 = 6;
+var num2 = 6;
+var answer;
+
+if (num1 > num2) {
+    answer = num1 - num2;
+    console.log("The answer is: " + answer);
+} else if (num1 < num2) {
+    answer = num2 - num1;
+    console.log("The answer is: " + answer);
+} else {
+    console.log("neither");
+}
+//8. Uncomment the variables below. Write an if condition that check num1 is greater than num2 AND that name is the same as the code variable. If it is true, the block code for the if should print out the following sentence: "Num1 is greater than Num2 and Name and Code are the same"
+//Write code below:
+var num1 = 10;
+var num2 = 3;
+var name1 = "code block";
+var code = "code block";
+if (num1 > num2 && name1 === code) {
+    console.log("Num1 is greater than Num2 and Name and Code are the same");
+}
+
+
+//9. Uncomment the code. Without changing the variables, alter the if condition to make the statement true.
+
+var num1 = 10;
+var num2 = 3;
+var name1 = "code block";
+var code = "what code?";
+
+if (num1 >= num2 & name1 !== code) {
+    console.log("Num1 is greater than Num2 and Name and Code are the same");
+}
+
+//10. You need to watch your curly braces with nested ifs. Uncomment the varables below. Write a if statement to check that the name variable is not the same as the code variable. The if body should contain another if condition to check if num1 is greater than num2. Print out the following sentence in the nest if body: "This is called an nested if"
+var num1 = 10;
+var num2 = 3;
+var name1 = "code block";
+var code = "what code?";
+if (name1 !== code) {
+    if (num1 > num2) {
+        console.log("This is called an nested if");
+    }
+}
+
+//11. Finally Rewrite the following code to replace the statement below with a ternary operator structure. You might want to copy the original code before altering just in case.
+
+var value1 = "world";
+var value2 = "hello";
+//
+//if(value1 === value2){
+//    console.log("Print " + value1 + " " + value2);
+//} else{
+//    console.log("Print " + value2 + " " + value1);
+//}
+value1 === value2 ? console.log("Print " + value1 + " " + value2) : console.log("Print " + value2 + " " + value1);
+
