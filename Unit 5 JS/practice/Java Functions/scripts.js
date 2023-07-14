79ca219f32ed28e01b2b3e9bbdfe398899a80d0b
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-14 14:22:46 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Unit 5 JS/practice/Java Functions/scripts.js

--- Unit 5 JS/practice/Java Functions/scripts.js
+++ Unit 5 JS/practice/Java Functions/scripts.js
@@ -1,97 +1,97 @@
-// // format for a fx
+// format for a fx
 
-// // -------------------regular fx-------------------
+// -------------------regular fx-------------------
 
-// var a = 5;
-// var b = 4;
-// function fxName (a,b) {
-//     var result = a * b;
-//     console.log(result);
-//     return result;
-// }
-// // call the fx and pass vars as args
-// fxName(a, b);
+var a = 5;
+var b = 4;
+function fxName (a,b) {
+    var result = a * b;
+    console.log(result);
+    return result;
+}
+// call the fx and pass vars as args
+fxName(a, b);
 
-// // ------------------anonymous fx----------------------
+// ------------------anonymous fx----------------------
 
-// // invoked by calling variable as a fx
-// var divided = function(){
-//     var result0 = 3/4;
-//     console.log("3 divided by 4 is", result0);
-// }
-// // call the variable
-// divided();
+// invoked by calling variable as a fx
+var divided = function(){
+    var result0 = 3/4;
+    console.log("3 divided by 4 is", result0);
+}
+// call the variable
+divided();
 
-// // ------------------immediately invoked function expression--------------
+// ------------------immediately invoked function expression--------------
 
-// // runs as soon as the the browser finds it
-// (function() {
-//     var result1 = 12 / 0.75;
-//     console.log("12 divided by 0.75 is", result1 + (","), "spacing on concat is not required when using a comma for args.");
-// }())
+// runs as soon as the the browser finds it
+(function() {
+    var result1 = 12 / 0.75;
+    console.log("12 divided by 0.75 is", result1 + (","), "spacing on concat is not required when using a comma for args.");
+}())
 
 // // ---------------------------basic fx's--------------------------------------
 
-// // individual lines
-// console.log('Arf');
-// console.log('Woof');
-// console.log('Meow');
-// console.log('Moooooooooo');
-// // regular fx
-// function speak(){
-//     console.log('Arf');
-//     console.log('Woof');
-//     console.log('Meow');
-//     console.log('Moooooooooo');
-// }
-// // call the fx can call from console also
-// speak();
+// individual lines
+console.log('Arf');
+console.log('Woof');
+console.log('Meow');
+console.log('Moooooooooo');
+// regular fx
+function speak(){
+    console.log('Arf');
+    console.log('Woof');
+    console.log('Meow');
+    console.log('Moooooooooo');
+}
+// call the fx can call from console also
+speak();
 
 // -------------------------build a basic fx---------------------------------------
 
-// // 1. define fx 
-// function findBiggestFraction(){
-//     // expression ? if : else
-//     a>b ? console.log("a: ", a) : console.log("b: ", b);
-// }
-// // 2. define vars
-// var a = 3/4;
-// var b = 5/7;
-// // call fx
-// findBiggestFraction();
+// 1. define fx 
+function findBiggestFraction(){
+    // expression ? if : else
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+// 2. define vars
+var a = 3/4;
+var b = 5/7;
+// call fx
+findBiggestFraction();
 
 // ----------------------pass args to the fx-----------------------
 
-//  // 1. define fx 
-// function findBiggestFraction(a, b){
-//     // expression ? if : else
-//     a>b ? console.log("a: ", a) : console.log("b: ", b);
-// }
-// // declare vars
-// var firstFraction = 3/4;
-// var secondFraction = 5/7;
-// // call fx, pass args in. Note firstFraction becomes a in the fx and secondFraction becomes b
-// findBiggestFraction(firstFraction, secondFraction);
-// // fx is reusable so you can pass in any args
-// findBiggestFraction(8/31, 9/36);
+ // 1. define fx 
+function findBiggestFraction(a, b){
+    // expression ? if : else
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
+}
+// declare vars
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+// call fx, pass args in. Note firstFraction becomes a in the fx and secondFraction becomes b
+findBiggestFraction(firstFraction, secondFraction);
+// fx is reusable so you can pass in any args
+findBiggestFraction(8/31, 9/36);
 
 // ------------------------return values from a fx---------------------
-// function findBiggestFraction(a, b) {
-//     var result;
-//     a > b ? result = ["firstFraction ", a] : result = ["secondFraction ", b];
-//     return result;
-// }
-// var firstFraction = 3 / 4;
-// var secondFraction = 5 / 7;
-// // call and print
-// console.log(findBiggestFraction(firstFraction, secondFraction));
-// // create and print var which calls fx and stores the return value
-// var fractionResult = findBiggestFraction(firstFraction, secondFraction);
-// console.log(fractionResult);
-// // can access the individual array results
-// console.log("First fraction result is:", firstFraction);
-// console.log("Second fraction result is:", secondFraction);
-// console.log("Fraction " + fractionResult[0] + "with a value of " + fractionResult[1] + " is the biggest.");
+function findBiggestFraction(a, b) {
+    var result;
+    a > b ? result = ["firstFraction ", a] : result = ["secondFraction ", b];
+    return result;
+}
+var firstFraction = 3 / 4;
+var secondFraction = 5 / 7;
+// call and print
+console.log(findBiggestFraction(firstFraction, secondFraction));
+// create and print var which calls fx and stores the return value
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
+console.log(fractionResult);
+// can access the individual array results
+console.log("First fraction result is:", firstFraction);
+console.log("Second fraction result is:", secondFraction);
+console.log("Fraction " + fractionResult[0] + "with a value of " + fractionResult[1] + " is the biggest.");
 
 // ------------------------fx setup and calls fx call in a fx----------------------------------------------------
 
