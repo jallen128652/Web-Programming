5ddb9b8b06082dbd1c0cb5e7d566154501de5671
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-29 10:07:35 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   Unit 6 JS/practice/images/dom.png
    	new file:   Unit 6 JS/practice/index.html
    	new file:   Unit 6 JS/practice/scripts.js

---
+++ Unit 6 JS/practice/scripts.js
@@ -0,0 +1,5 @@
+// BOM commands
+window.innerWidth // gives width of window in px
+window.open() // opens new tab
+// DOM commands and notes
+window.document // calls the document Document has it's own object module.
