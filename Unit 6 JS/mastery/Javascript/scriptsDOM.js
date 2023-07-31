1c307be2e0c820afbb2f7e73f0b8a9a032e8ad83
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-31 12:47:32 -0500

    Changes to be committed: 	new file:   Unit 6 JS/mastery/Images/cadDesignSmall-500.jpg 	new file:   Unit 6 JS/mastery/Images/codingSmall-500.jpg 	new file:   Unit 6 JS/mastery/Images/roboticsSmall-500.jpg 	modified:   Unit 6 JS/mastery/Javascript/scriptsDOM.js

--- Unit 6 JS/mastery/Javascript/scriptsDOM.js
+++ Unit 6 JS/mastery/Javascript/scriptsDOM.js
@@ -1,3 +1,4 @@
+// ------------------modify body paragraph----------------------------
 // get the text and store it
 const introText = document.querySelector(".intro p").innerHTML;
 // create new text and store it
@@ -7,5 +8,18 @@ const newPrint = document.querySelector(".intro p");
 // assign new text to the text content of the element
 newPrint.textContent = newText;
 
+// ------------------modify pics and figcaptions--------------------------
+// query the images by id to get url
+const firstPic = document.getElementById("firstImage").src;
+const secondPic = document.getElementById("secondImage").src;
+const thirdPic = document.getElementById("thirdImage").src;
+console.log(firstPic);
+console.log(secondPic);
+console.log(thirdPic);
+// setup array for image titles
+let titles = document.querySelectorAll(".content-title").innerHTML; //not correct yet
+// change the paragraph for each image and adjust css to fit
 
+// ------------------modify what each button does----------------------------
+// add click event listener
 
