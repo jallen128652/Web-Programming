539074a3b592b63382f556af93c2db94407e58ef
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-31 10:32:19 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Unit 6 JS/mastery/Javascript/scriptsDOM.js
    	modified:   Unit 6 JS/mastery/notes for the mastery.txt

---
+++ Unit 6 JS/mastery/Javascript/scriptsDOM.js
@@ -0,0 +1,11 @@
+// get the text and store it
+const introText = document.querySelector(".intro p").innerHTML;
+// create new text and store it
+const newText = "Welcome to TSTC. We have an amazing program with the opportunity to give you life changing skills. Whatever your goals are, we can assist you with reaching them. We pride ourselves in our students' success!";
+// new const to store the text into the element
+const newPrint = document.querySelector(".intro p");
+// assign new text to the text content of the element
+newPrint.textContent = newText;
+
+
+
