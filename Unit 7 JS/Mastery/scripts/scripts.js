f931862c55ec7f1ff0c3e5001b3350015a85132a
jallen128652 <jallen128652@mymail.tstc.edu>
2023-08-11 16:16:18 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Unit 7 JS/Mastery/CSS/styles.css
    	new file:   Unit 7 JS/Mastery/images/crewblack.jpg
    	new file:   Unit 7 JS/Mastery/images/crewgray.jpg
    	new file:   Unit 7 JS/Mastery/images/crewred.jpg
    	new file:   Unit 7 JS/Mastery/images/hoodieblack.jpg
    	new file:   Unit 7 JS/Mastery/images/hoodiegray.jpg
    	new file:   Unit 7 JS/Mastery/images/hoodiered.jpg
    	new file:   Unit 7 JS/Mastery/images/sweaterblack.jpg
    	new file:   Unit 7 JS/Mastery/images/sweatergray.jpg
    	new file:   Unit 7 JS/Mastery/images/sweaterred.jpg
    	new file:   Unit 7 JS/Mastery/images/vneckblack.jpg
    	new file:   Unit 7 JS/Mastery/images/vneckgray.jpg
    	new file:   Unit 7 JS/Mastery/images/vneckred.jpg
    	modified:   Unit 7 JS/Mastery/index.html
    	modified:   Unit 7 JS/Mastery/order.html
    	modified:   Unit 7 JS/Mastery/scripts/scripts.js

--- Unit 7 JS/Mastery/scripts/scripts.js
+++ Unit 7 JS/Mastery/scripts/scripts.js
@@ -1,6 +1,6 @@
 // event listener calls fx 1 when the submit button is pressed
 document.getElementById('order').addEventListener('submit', outerMain);
-// function 1
+// function 1 pass arg for submit button press
 function outerMain(event) {
     // prevent default behavior on load
     event.preventDefault();
@@ -9,7 +9,7 @@ function outerMain(event) {
     // evaluate and move to the calc fx call
     if (validated === true) {
         // recieve total from fx and store in the total var
-        var total = calculateTotal();
+        var total = calculateTotal(total);
         //  declare vars for the order   
         // print contact details for the customer    
         document.getElementById("oname").innerHTML = ("Customer name is: " + document.getElementById('name').value);
@@ -19,7 +19,6 @@ function outerMain(event) {
             document.getElementById('state').value + ". " + document.getElementById('zip').value);
         document.getElementById("ophone").innerHTML = ("Phone number: " + document.getElementById('phone').value);
         document.getElementById("ototal").innerHTML = ("Your total is: $" + total.toFixed(2));
-
     }
 }
 // fx 2 called from fx1
@@ -46,7 +45,7 @@ function validateForm() {
     }
 }
 // fx3 called ffrom fx1
-function calculateTotal() {
+function calculateTotal(total) {
     var state = document.getElementById('state');
     // vars that store boolean vals if checked or not
     var crew = document.getElementById('crew').checked;
@@ -59,7 +58,7 @@ function calculateTotal() {
     var qty4 = 0;
     var tax, shipping, total, subTotal;
     const RATE = 0.0825;
-
+    //  grab values and store in vars
     if (crew === true) {
         var item1 = parseInt(document.getElementById('crew').value, 10) || 0;
         qty1 = parseInt(document.getElementById("qty1").value, 10) || 0;
@@ -84,6 +83,38 @@ function calculateTotal() {
     } else {
         item4 = 0;
     }
+    // stores item qty's, sz's and colors in array
+    var clr1 = document.getElementById("color1").value;
+    var clr2 = document.getElementById("color2").value;
+    var clr3 = document.getElementById("color3").value;
+    var clr4 = document.getElementById("color4").value;
+
+    var sz1 = document.getElementById("size1").value;
+    var sz2 = document.getElementById("size2").value;
+    var sz3 = document.getElementById("size3").value;
+    var sz4 = document.getElementById("size4").value;
+
+    var items = [];
+    //  grab values and store in array
+    if (crew === true) {
+        items.push(qty1, " ", sz1, " ", clr1, " Crew Neck Tshirts @ $", item1, ".00, ");
+    }
+    if (vneck === true) {
+        items.push(qty2, " ", sz2, " ", clr2, " V Neck Tshirts @ $", item2, ".00, ");
+    }
+    if (sweater === true) {
+        items.push(qty3, " ", sz3, " ", clr3, " Sweaters @ $", item3, ".00, ");
+    }
+    if (hoodie === true) {
+        items.push(qty4, " ", sz4, " ", clr4, " Hoodies @ $", item4, ".00, ");
+    }
+    // print items ordered to receipt from array loop
+    let orders = "";
+    for (i = 0; i < items.length; i++) {
+        orders += items[i];
+        document.getElementById("oColorQtyType").innerHTML = (orders);
+    }
+
     // adds up the subtotal 
     subTotal = ((item1 * qty1) + (item2 * qty2) + (item3 * qty3) + (item4 * qty4));
     // calcs tax
@@ -105,29 +136,11 @@ function calculateTotal() {
             shipping = 0;
             break;
     }
-    // testing
-    console.log(item1);
-    console.log(item2);
-    console.log(item3);
-    console.log(item4);
-    console.log(qty1);
-    console.log(qty2);
-    console.log(qty3);
-    console.log(qty4);
-    console.log(subTotal);
-    console.log(tax);
-    console.log(shipping);
-
-
+    // print some values to receipt
+    document.getElementById("sub").innerHTML = ("Sub Total: $" + subTotal.toFixed(2));
+    document.getElementById("tax").innerHTML = ("Taxes: $" + tax.toFixed(2));
+    document.getElementById("ship").innerHTML = ("Shipping: $" + shipping.toFixed(2))
     total = subTotal + tax + shipping;
-    // testing
-    console.log(total);
-    return total;
-}
-// still need:
-// array
-// loop
 
-// want:
-// incorporate color drop down to change image
-// store order values in array and loop through the array to display order details for qty/type/color
+    return total;
+} 
